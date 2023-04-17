6c070401af02a32c24c471a8f0433cd50b752607
Terezia Banasova <251010@vutbr.cz>
2023-04-17 09:32:16 +0200

    sorting

--- sorting.py
+++ sorting.py
@@ -21,10 +21,24 @@ def read_data(file_name):
                     data[header].append(int(value))
     return data
 
+
+def selection_sort(number_array):
+    n = len(number_array)
+    for i in range(n):
+        min_idx = i
+        for num_idx in range(i + 1, n):
+            if number_array[num_idx] < number_array[min_idx]:
+                min_idx = num_idx
+        number_array[i], number_array[min_idx] = number_array[min_idx], number_array[i]
+    return number_array
+
+
+
 def main():
     my_data = read_data("numbers.csv")
     print(my_data)
-
+    selection_sort_result = selection_sort(my_data["series_1"].copy())
+    print(selection_sort_result)
 
 if __name__ == '__main__':
     main()
