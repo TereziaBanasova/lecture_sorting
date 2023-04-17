b864eca482ab00d6c34a4d0d4f6a2c29dc083a2c
Terezia Banasova <251010@vutbr.cz>
2023-04-17 10:24:38 +0200

    sorting

--- sorting.py
+++ sorting.py
@@ -32,13 +32,36 @@ def selection_sort(number_array):
         number_array[i], number_array[min_idx] = number_array[min_idx], number_array[i]
     return number_array
 
+def bubble_sort(list_of_numbers):
+    n = len(list_of_numbers)
+    for i in range(n-1):
+        for idx in range(n - i - 1):
+            if list_of_numbers[idx] > list_of_numbers[idx+1]:
+                list_of_numbers[idx], list_of_numbers[idx+1] = list_of_numbers[idx+1], list_of_numbers[idx]
+    return list_of_numbers
 
+def insertion_sort(array):
+    for step in range(1, len(array)):
+        key = array[step]
+        i = step - 1
+        while i >= 0 and key < array[i]:
+            array[i + 1] = array[i]
+            i = i - 1
+        array[i + 1] = key
+    return array
 
 def main():
     my_data = read_data("numbers.csv")
     print(my_data)
+
     selection_sort_result = selection_sort(my_data["series_1"].copy())
     print(selection_sort_result)
+
+    bubble_sort_result = bubble_sort(my_data["series_1"].copy())
+    print(bubble_sort_result)
+
+    insertion_sort_result = insertion_sort(my_data["series_1"].copy())
+    print(insertion_sort_result)
 
 if __name__ == '__main__':
     main()
